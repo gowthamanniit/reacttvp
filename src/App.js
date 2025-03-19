00525414ef9f25dca1a650dd59927f7f0cb471ac
gowthamanniit <karurgowthaman@gmail.com>
2025-03-19 14:02:29 +0530

    destructing loop map

--- src/App.js
+++ src/App.js
@@ -1,3 +1,220 @@
+function App()
+{
+  var arr=[11,23,33,44,55,66,77,88,99,100,200,300]
+  return(
+    <>
+    <h2>all elements</h2>
+    <ol type="I">
+        {arr.map((item)=><li>{item}</li>)}
+      </ol>
+      <h2>even/odd elements</h2>
+      <ul type="circle">
+        {arr.map((item)=>
+            <li>
+            {(item%2===0 && item+" even ") || item+" odd" }
+            </li>    
+        )}
+      </ul>
+
+      <h2>even only</h2>
+      <ul type="square">
+        {arr.map((item)=>      
+            <>
+            {item%2===0 && <li>{item}</li>}            
+            </>      
+        )}
+      </ul>
+
+    </>
+  )
+}
+export default App
+/*
+function App()
+{
+  var sobj={
+    rno:19991,
+    sname:"ramesh",
+    marks:{
+      tamil:88,
+    english:83,
+    maths:99
+  }
+  }
+  const dis=({rno,sname,marks:{tamil,english,maths}})=>{
+  return [rno,sname,tamil,english,maths]
+  }
+  var [v1,v2,v3,v4,v5]=dis(sobj)
+  return(
+    <>
+    <h1>Nested Object Destructing</h1>
+    <h1>Roll Number:{v1}</h1>
+    <h1>Student Name:{v2}</h1>
+    <h1>Tamil Mark:{v3}</h1>
+    <h1>English Mark:{v4}</h1>
+    <h1>Maths Mark:{v5}</h1>
+    </>
+  )
+}
+export default App
+/*
+function Gowthaman()
+{
+  const show=(a,b)=>
+  {
+    var add=a+b
+    var sub=a-b
+    var mul=a*b
+    var div=a/b
+    return [add,sub,mul,div]
+  }
+  var [v1,v2,v3,v4]=show(11,22)
+  return(<>
+  <h1>Function Destructing</h1>
+    Addition:{v1} <br></br>
+    Subtraction:{v2} <br></br>
+    Multiplication:{v3} <br></br>
+    Division:{v4} <br></br>
+  </>)
+}
+export default Gowthaman
+/*
+function Gowthaman()
+{
+  var [v1,,,v2,...v3]=[11,22,33,44,55,66,77]
+  return(
+    <>
+    <h1>Array Destructing: To get data from source</h1>
+      value 1 :{v1}
+      <br></br>
+      value 2 :{v2}
+      <br></br>
+      value 3 :{v3}
+    
+    </>
+  )
+}
+export default Gowthaman
+/*
+function Gowthaman()
+{
+  const show=(a,b,c)=>{
+    
+    if(true)
+    {
+    var k=100
+    k++
+    }
+    document.getElementById("r1").innerHTML="arrow show function"+(a+b+c+k)
+  }
+  function dis(a,b,c)
+  {
+  document.getElementById("r2").innerHTML="normal dis function"+(a+b+c)
+  }
+  return(
+    <>
+    <h1>arrow function without parameter</h1>
+    
+         <button onClick={()=>show(10,20,30)}>click me</button>
+         <button onClick={()=>dis(11,22,33)}>click me</button>
+     <div id="r1"></div>
+     <div id="r2"></div>
+    </>
+  )
+}
+export default Gowthaman
+/*
+function Gowthaman()
+{
+  const show=()=>{
+    document.getElementById("r1").innerHTML="arrow show function"
+  }
+  function dis()
+  {
+  document.getElementById("r2").innerHTML="normal dis function"
+  }
+  return(
+    <>
+    <h1>arrow function without parameter</h1>
+    <button onClick={dis}>click me</button>
+    <button onClick={()=>dis()}>click me</button>
+
+     <button onClick={show}>click me</button>
+     <div id="r1"></div>
+     <div id="r2"></div>
+    </>
+  )
+}
+export default Gowthaman
+/*
+function Gowthaman()
+{
+  var stud=[{rno:1001,sname:"preethi",mark:67},
+            {rno:1002,sname:"vignesh",mark:77},
+            {rno:1003,sname:"prabha",mark:87}
+          ]  
+  
+    return(
+      <>
+      <h1>Roll Number:{stud[0].rno}</h1>      
+      <h1>Sname:{stud[0].sname}</h1>
+      <h1>Mark:{stud[0].mark}</h1>
+      <h1>Roll Number:{stud[1].rno}</h1>      
+      <h1>Sname:{stud[1].sname}</h1>
+      <h1>Mark:{stud[1].mark}</h1>
+      <h1>Roll Number:{stud[2].rno}</h1>      
+      <h1>Sname:{stud[2].sname}</h1>
+      <h1>Mark:{stud[2].mark}</h1>
+      
+      </>
+    )
+}
+export default Gowthaman
+
+/*
+function Gowthaman()
+{
+  var stud={
+    rno:1001,
+    sname:"preethi",
+    subjects:{mark1:88,
+    mark2:98,
+    mark3:45}
+  }
+    return(
+      <>
+      <h1>Roll Number:{stud.rno}</h1>
+      <h1>Student Name:{stud.sname}</h1>
+      <h1>Tamil:{stud.subjects["mark1"]}</h1>
+      <h1>English:{stud.subjects["mark2"]}</h1>
+      <h1>Maths:{stud.subjects["mark3"]}</h1>
+      </>
+    )
+}
+export default Gowthaman
+
+/*
+function Gowthaman()
+{
+  var stud={
+    rno:1001,
+    sname:"preethi",
+    mark1:88,
+    mark2:98,
+    mark3:45
+  }
+    return(
+      <>
+      <h1>Roll Number:{stud.rno}</h1>
+      <h1>Student Name:{stud.sname}</h1>
+      <h1>Tamil:{stud.mark1}</h1>
+      <h1>English:{stud.mark2}</h1>
+      <h1>Maths:{stud.mark3}</h1>
+      </>
+    )
+}
+export default Gowthaman
+/*
 function Gowthaman()
 {
   // var arr=new Array(11,22,33,44,55,66,77)
